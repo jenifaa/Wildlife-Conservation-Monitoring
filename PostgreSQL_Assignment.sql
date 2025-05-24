011e42bf403f9f818ac259dd060f4befc12446af
jenifaa <jefrahman32@gmail.com>
2025-05-25 01:20:14 +0600

    added first problem solution

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,58 @@
 -- Active: 1747599526712@@127.0.0.1@5432@conservation_db
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(100) DEFAULT 'Unknown'
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers (ranger_id),
+    species_id INT REFERENCES species (species_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
+-- DELETE FROM rangers;
+-- DELETE FROM species;
+-- DELETE FROM rangers;
+
+INSERT INTO
+    rangers (ranger_id, name, region)
+VALUES (
+        1,
+        'Alice Green',
+        'Northern Hills'
+    ),
+    (2, 'Bob White', 'River Delta'),
+    (
+        3,
+        'Carol King',
+        'Mountain Range'
+    );
+    INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+SELECT * FROM rangers;
+
+SELECT * FROM species;
+
+SELECT * FROM sightings;
+
+-- problem-1
+INSERT INTO
+    rangers (ranger_id,name, region)
+VALUES (4,'Derek Fox', 'Coastal Plains');
